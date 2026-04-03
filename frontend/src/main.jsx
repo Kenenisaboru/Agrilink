f2c26ddb86d179173cbe4e1a88aaa6dad0fc4275
kenenisa <kenenisaboru998@gmail.com>
2026-04-03 19:22:09 +0100

    Refactor main.jsx for improved app initialization

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext.jsx'
+
+axios.defaults.baseURL = 'http://localhost:5000';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
