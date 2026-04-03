136b4f311a38256e1253585dc50d33c1d43a9903
kenenisa <kenenisaboru998@gmail.com>
2026-04-03 19:57:46 +0100

    Fix auth middleware logic

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -31,13 +31,16 @@ const protect = async (req, res, next) => {
   }
 };
 
-// Check if user is an Admin
-const admin = (req, res, next) => {
-  if (req.user && req.user.role === 'Admin') {
+// Check for specific roles
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: `User role ${req.user.role} is not authorized to access this route`,
+      });
+    }
     next();
-  } else {
-    res.status(401).json({ message: 'Not authorized as an admin' });
-  }
+  };
 };
 
-module.exports = { protect, admin };
+module.exports = { protect, admin, authorize };
