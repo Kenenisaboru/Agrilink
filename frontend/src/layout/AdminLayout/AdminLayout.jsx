981b184ab29c9dd7f87ecc3ca8fdfdd45c9cec9a
kenenisa <kenenisaboru998@gmail.com>
2026-04-07 07:32:35 +0100

    Update Admin layout

--- frontend/src/layout/AdminLayout/AdminLayout.jsx
+++ frontend/src/layout/AdminLayout/AdminLayout.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Sidebar from '../../components/common/Sidebar';
 import Navbar from '../../components/common/Navbar';
+import Footer from '../../components/Footer';
 
 const AdminLayout = ({ children }) => {
   return (
-    <div className="flex h-screen bg-gray-50 overflow-hidden">
+    <div className="flex min-h-screen bg-gray-50">
       <Sidebar role="Admin" />
-      <div className="flex-1 flex flex-col overflow-hidden">
+      <div className="flex-1 flex flex-col">
         <Navbar />
         <main className="flex-1 overflow-y-auto p-6">
           {children}
         </main>
+        <Footer />
       </div>
     </div>
   );
