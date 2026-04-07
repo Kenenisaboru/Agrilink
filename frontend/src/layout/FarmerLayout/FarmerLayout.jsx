bdcf0250a232f707f676f4caeb64d1985e8675cd
kenenisa <kenenisaboru998@gmail.com>
2026-04-07 07:33:03 +0100

    Update Farmer layout

--- frontend/src/layout/FarmerLayout/FarmerLayout.jsx
+++ frontend/src/layout/FarmerLayout/FarmerLayout.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Sidebar from '../../components/common/Sidebar';
 import Navbar from '../../components/common/Navbar';
+import Footer from '../../components/Footer';
 
 const FarmerLayout = ({ children }) => {
   return (
-    <div className="flex h-screen bg-gray-50 overflow-hidden">
+    <div className="flex min-h-screen bg-gray-50">
       <Sidebar role="Farmer" />
-      <div className="flex-1 flex flex-col overflow-hidden">
+      <div className="flex-1 flex flex-col">
         <Navbar />
         <main className="flex-1 overflow-y-auto p-6">
           {children}
         </main>
+        <Footer />
       </div>
     </div>
   );
