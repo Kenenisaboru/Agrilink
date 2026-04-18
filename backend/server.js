a3c2fede7f9810b073667b487e39f8db8ff61c5f
kenenisa <kenenisaboru998@gmail.com>
2026-04-18 20:45:07 +0100

    Update CORS policy to allow local network access (for mobile testing)

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
       'http://localhost:5173',
       'http://localhost:5174',
       'http://localhost:3000',
+      'http://192.168.137.160:5173',
     ];
 
 const io = socketio(server, {
