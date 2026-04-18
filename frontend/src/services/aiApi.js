c5460d5554b19736a50aab31cd94db9e354b0872
kenenisa <kenenisaboru998@gmail.com>
2026-04-18 20:45:41 +0100

    Update API connection URLs to use network IP for mobile support

--- frontend/src/services/aiApi.js
+++ frontend/src/services/aiApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'http://192.168.137.160:5001/api';
 
 export const chatWithAI = async (message) => {
   try {
