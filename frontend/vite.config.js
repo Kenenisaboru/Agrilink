f128776fd923e861aad50ade2a4c46015191eaba
kenenisa <kenenisaboru998@gmail.com>
2026-04-20 15:52:26 +0100

    chore: update Vite build configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -51,6 +51,10 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/flask-api/, '/api'),
       },
+      '/uploads': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
     },
   },
 })
