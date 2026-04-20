1a5ba64d418d361db56533e6df27ab76f8467ed3
kenenisa <kenenisaboru998@gmail.com>
2026-04-20 15:59:04 +0100

    feat: enhance service worker behavior

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.k4soe99l5ao"
+    "revision": "0.h63c910adgg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
