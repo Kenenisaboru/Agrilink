76096fccdcab49eeadcd4d67ad10651a1228ff26
kenenisa <kenenisaboru998@gmail.com>
2026-05-07 20:14:26 +0100

    Fix farmer layout scroll behavior with h-screen container

--- frontend/src/layout/FarmerLayout/FarmerLayout.jsx
+++ frontend/src/layout/FarmerLayout/FarmerLayout.jsx
@@ -4,9 +4,9 @@ import Navbar from '../../components/common/Navbar';
 
 const FarmerLayout = ({ children }) => {
   return (
-    <div className="flex min-h-screen bg-gray-50">
+    <div className="flex h-screen overflow-hidden bg-gray-50">
       <Sidebar role="Farmer" />
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
         <Navbar />
         <main className="flex-1 overflow-y-auto p-6">
           {children}
